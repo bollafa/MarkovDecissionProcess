757d92102e8a63d21c301419c73a509a1d397095
Bollafa <gudo39@gmail.com>
2018-02-01 20:53:54 +0100

    Finish value_iteration

--- main.jl
+++ main.jl
@@ -4,24 +4,34 @@
 =#
 
 using DataStructures
+using IterTools
 function value_iteration(S,A,P,R,Θ)
-γ = 0
-V₀ = DefaultDict{Int,Int}(0)
-Vk = [V₀]
-V₁ = DefaultDict(0)
+γ = 0.9
+V₀ = DefaultDict{Tuple{Int,Int},Int}(0)
+Vk::Array{Any} = [V₀]
+k = 1
+V₁::Dict{Tuple{Int,Int},Float64} = Dict{Tuple{Int,Int},Float64}()
 # update value
-for s in S
-    Vp = []
-    for a in A
-        #@show S
-        println(broadcast(s′ -> R(s,a,s′),S))
-        println(broadcast(s′ -> P(s′,s,a),S))
-        push!(Vp,sum(broadcast(s′ -> P(s′,s,a)*R(s,a,s′),S)))
+while true
+    k = k + 1
+    push!(Vk,Dict{Tuple{Int,Int},Float64}())
+    for s in S
+        Vp = []
+         for a in A
+            #=@show s,a
+            println(broadcast(s′ -> R(s,a,s′),[S...]))
+            println(broadcast(s′ -> P(s′,s,a),[S...]))=#
+            #push!(Vp,sum(broadcast(s′ -> P(s′,s,a)*R(s,a,s′),[S...])))
+            push!(Vp, sum(broadcast(s′ -> P(s′,s,a)*(R(s,a,s′)+γ*Vk[k-1][s′]),[S...])))
+        end
+        #@show Vp
+        Vk[k][s] = maximum(Vp)
+
     end
-    @show Vp
-    V₁[s] = maximum(Vp)
+    abs(sum(abs,values(Vk[k])) - sum(abs,values(Vk[k-1]))) < Θ && break;
 end
-V₁
+@show k
+Vk[k]
 end
 ActionToVec = Dict(1 => (0,1),2 => (1,0), 3 => (0,-1), 4 => (-1,0))
 function P(s′,s,a)
@@ -36,7 +46,7 @@ function P(s′,s,a)
 
 end
 function R(s,a,s′)
-    s == (0,0) && return 2
-    s == (1,1) && return -2
+    s == (0,0) && return 10
+    s == (1,1) && return -10
     0
 end
